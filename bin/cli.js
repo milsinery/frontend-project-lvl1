d6abae808136956b1d31760816abfe1a277f1864
Vladimir Bykov <milsinery@icloud.com>
2022-04-03 17:43:55 +0300

    add eslint

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const askNameAndWelcome = () => {
+const askNameAndWelcome = () => {
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name.length > 0 ? name : 'friend'}!`);
 };
+
+export default askNameAndWelcome;
